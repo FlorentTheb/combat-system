4ff2dffa86bb61b775d9ba35d1b185964610fc88
FlorentTheb <florenthebault@gmail.com>
2026-04-16 18:09:43 +0200

    Add a handler for future communications between each client and the endpoint of the sock server side

---
+++ src/main/java/com/ankama/ClientServerHandler.java
@@ -0,0 +1,35 @@
+package com.ankama;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class ClientServerHandler implements Runnable {
+
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private Socket socket;
+
+    public ClientServerHandler(Socket socket) {
+        this.socket = socket;
+        try {
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+    
+}
